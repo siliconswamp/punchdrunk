fe166cb9ed59abf641d32627be74bc6c07edc7a4
ahillebra <41175151+ahillebra@users.noreply.github.com>
2019-04-01 15:31:36 -0500

    Assets from most recent Unity Package

---
+++ Assets/Scripts/EnemyObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[System.Serializable]
+public class EnemyObject : MonoBehaviour
+{
+    public bool isDead;
+    public bool Guard;
+    public bool Evasion;
+
+    public Image EnemyHPProgressBar;
+    public Image EnemyStaminaProgressBar;
+
+    [HideInInspector]
+    public float baseHP, currentHP, baseStamina, currentStamina, HPpercentage, staminaPercentage;
+
+    public void Start()
+    {
+        baseHP = 200;
+        baseStamina = 100;
+        currentHP = baseHP;
+        currentStamina = baseStamina;
+        currentHP = Mathf.Clamp(currentHP, 0, 200);
+        currentStamina = Mathf.Clamp(currentStamina, 0, 100);
+    }
+
+    public void enemyUpdateBars()
+    {
+        HPpercentage = (float)currentHP / baseHP;
+        EnemyHPProgressBar.fillAmount = HPpercentage;
+        staminaPercentage = (float)currentStamina / baseStamina;
+        EnemyStaminaProgressBar.fillAmount = staminaPercentage;
+    }
+
+    public void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/MatchTurnEnemy.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Don
+[System.Serializable]
+public class MatchTurnEnemy : MonoBehaviour
+{
+    public PlayerObject player;
+    public EnemyObject enemy;
+    public MatchTurn turn;
+    public BaseAttack enemyAtk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void doEnemyTurn()
+    {
+        Debug.Log("Enemy state: In enemy turn");
+
+        if (enemy.currentHP <= 0)
+        {
+            enemy.isDead = true;
+            turn.PlayerTurn = false;
+            turn.EnemyTurn = false;
+        }
+        else
+        {
+            enemy.Guard = false;
+
+            if (player.currentHP <= 0)
+            {
+                player.isDead = true;
+                turn.PlayerTurn = false;
+                turn.EnemyTurn = false;
+            }
+        }
+        EnemyMove1();
+        yieldETurn();
+    }
+
+    public void EnemyMove1()
+    {
+        Debug.Log("Enemy state: In EnemyMove1");
+        enemyAtk.attackDmg = 10;
+        enemyAtk.attackStm = 5;
+        player.currentHP -= enemyAtk.attackDmg;
+        enemy.currentStamina -= enemyAtk.attackStm;
+        enemy.enemyUpdateBars();
+        player.playerUpdateBars();
+    }
+
+    public void yieldETurn()
+    {
+        Debug.Log("Enemy state: In yieldETurn");
+        turn.PlayerTurn = true;
+        turn.EnemyTurn = false;
+    }
+    // EnemyMove2,3,4
+}
