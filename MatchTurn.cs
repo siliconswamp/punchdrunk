f5e97b7a276f2de57c41ce81a70b03466bf1ce36
ahillebra <41175151+ahillebra@users.noreply.github.com>
2019-04-01 15:23:04 -0500

    Changed Scripts/First Uplaod
    MatchTurnEnemy and EnemyObject done by Don. Includes Josh's work from Skype session to include applying objects to BattleManager and changing my scripts which include all except for enemy scripts.

---
+++ MatchTurn.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[System.Serializable]
+public class MatchTurn : MonoBehaviour 
+{
+    public MatchTurnPlayer pScript;
+    public MatchTurnEnemy eScript;
+    public PlayerObject player;
+    public EnemyObject enemy;
+    public SceneChanger scene;
+
+    public bool PlayerTurn, EnemyTurn;
+    public object prevTurn;
+
+    public void Start()
+    {
+        if (prevTurn == null)
+        {
+            setUpGame();
+        }
+        doTurn();
+    }
+
+    public void setUpGame()
+    {
+        PlayerTurn = true;
+        EnemyTurn = false;
+    }
+
+    public void doTurn()
+    {
+        if (PlayerTurn)
+        {
+            pScript.doPlayerTurn();
+            prevTurn = pScript;
+        }
+        else if (EnemyTurn)
+        {
+            eScript.doEnemyTurn();
+            prevTurn = eScript;
+        }
+        else if (PlayerTurn == false && EnemyTurn == false)
+        {
+            if (player.isDead)
+            {
+                //Lose game
+                scene.lose();
+            }
+            else if (enemy.isDead)
+            {
+                //Win game
+                scene.win();
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+}
