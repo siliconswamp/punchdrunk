65375b372a9e807a595a33795cd12f2bea8b2b17
Ashleigh Hillebrand <41175151+ahillebra@users.noreply.github.com>
2019-04-11 23:55:17 -0500

    More Options for menu/More scenes

--- SceneChanger.cs
+++ SceneChanger.cs
@@ -5,6 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public void SceneSelector(string scene)
+    {
+        if (scene.Equals("main"))
+        {
+            SceneManager.LoadScene("MAIN");
+        }
+
+        if (scene.Equals("play"))
+        {
+            SceneManager.LoadScene("FIGHT");
+        }
+
+        if (scene.Equals("instruct"))
+        {
+            SceneManager.LoadScene("INSTRUCT");
+        }
+
+        if (scene.Equals("character"))
+        {
+            SceneManager.LoadScene("CHARSELECT");
+        }
+
+        if (scene.Equals("feat"))
+        {
+            SceneManager.LoadScene("TROPHY");
+        }
+
+        if (scene.Equals("round2"))
+        {
+            SceneManager.LoadScene("FIGHT2");
+        }
+
+        if (scene.Equals("round3"))
+        {
+            SceneManager.LoadScene("FIGHT3");
+        }
+        
+
+        if (scene.Equals("exit"))
+        {
+            //Works in editor only
+            //UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+    }
     public void mainMenu()
     {
         SceneManager.LoadScene("MAIN");
@@ -15,13 +60,30 @@ public class SceneChanger : MonoBehaviour
         SceneManager.LoadScene("FIGHT");
     }
 
+    public void help()
+    {
+        SceneManager.LoadScene("INSTRUCT");
+    }
+
+    public void charsel()
+    {
+        SceneManager.LoadScene("CHARSELECT");
+    }
+
+    public void achieve()
+    {
+        SceneManager.LoadScene("TROPHY");
+    }
+
     public void lose()
     {
+        Debug.Log("Player Lost Scene");
         SceneManager.LoadScene("LOSE");
     }
 
     public void win()
     {
+        Debug.Log("Player Won Scene");
         SceneManager.LoadScene("WIN");
     }
 
